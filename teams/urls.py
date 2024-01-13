b53d39429ba1f9c0f23611e9c504bc207775f20a
Volodymyr <volodimirkomarnickij28@gmail.com>
2024-01-13 20:40:16 +0200

    Implemented basic view and serializer for Team model. Added endpoint for it

---
+++ teams/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from rest_framework import routers
+
+from teams.views import TeamViewSet
+
+router = routers.DefaultRouter()
+router.register("teams", TeamViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "teams"
--- teams/views.py
+++ teams/views.py
@@ -1,2 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
+from teams.models import Team
+
+from teams.serializers import TeamSerializer
+
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
