d4577b7e6b2029891b919c19eee4f35c1b424f59
Volodymyr <volodimirkomarnickij28@gmail.com>
2024-01-14 02:50:45 +0200

    Added fields for first and last names for user

--- user/models.py
+++ user/models.py
@@ -35,8 +35,10 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=30)
+    last_name = models.CharField(_("last name"), max_length=30)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     objects = UserManager()
