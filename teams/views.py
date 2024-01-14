15a31a40361988a3af315285f3a6bbdaecc00513
Volodymyr <volodimirkomarnickij28@gmail.com>
2024-01-14 02:55:48 +0200

    Implemented viewset for Teams

--- teams/views.py
+++ teams/views.py
@@ -1,10 +1,30 @@
 from rest_framework import viewsets
 
 from teams.models import Team
-
-from teams.serializers import TeamSerializer
+from teams.serializers import (
+    TeamSerializer,
+    TeamDetailSerializer,
+    TeamListSerializer
+)
 
 
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action in ("list", "retrieve"):
+            queryset = queryset.prefetch_related("members")
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TeamListSerializer
+
+        if self.action == "retrieve":
+            return TeamDetailSerializer
+
+        return TeamSerializer
