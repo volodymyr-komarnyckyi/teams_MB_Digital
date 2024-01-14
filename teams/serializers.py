de2214a28e8c465c6528fc80ef7c0f08a30163c8
Volodymyr <volodimirkomarnickij28@gmail.com>
2024-01-14 02:55:03 +0200

    Implemented serializers for Team list and detail pages

--- teams/serializers.py
+++ teams/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from teams.models import Team
+from user.serializers import UserShowSerializer
 
 
 class TeamSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Team
-        fields = "__all__"
+        fields = ["id", "name", "members"]
+
+
+class TeamDetailSerializer(TeamSerializer):
+    members = UserShowSerializer(many=True, read_only=True)
+
+
+class TeamListSerializer(TeamSerializer):
+    members = serializers.SlugRelatedField(many=True, read_only=True, slug_field="email")
